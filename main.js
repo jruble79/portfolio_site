bad24a4033305967f720c5310e4da05207878380
Jeff Ruble <jeff.ruble@gmail.com>
2021-02-25 15:13:59 -0500

    Initial commit with toggleList function

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+// Show/Hide Resume Sections
+
+function toggleList() {
+    const section = document.getElementById('section');
+
+    if (section.style.display == 'block') {
+        section.style.display = 'none';
+    } else {
+        section.style.display = 'block';
+    }
+
+}
+
